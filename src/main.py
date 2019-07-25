07f8af1219cbc36833df6fd3ce5ac433eb65d901
cyclowns <notzombiedude@gmail.com>
2019-07-24 23:18:52 -0700

    starting installer + more RPC stuff

--- src/main.py
+++ src/main.py
@@ -11,8 +11,17 @@ from valve.source import NoResponseError
 import time
 import random
 
+# globals
+server_ip = ""
+
 RPC = Presence(client_id)
 RPC.connect()
+
+def parse_console_log():
+    pass
+
+def clear_console_log():
+    pass
 
 # Converts a name of a map (i.e. koth_viaduct) to its image value in the Rich Presence app (viaduct)
 def map_name_to_map_image(name):
@@ -22,24 +31,32 @@ def map_name_to_map_image(name):
 # Main loop
 while True:
     try:
-        with ServerQuerier(("167.114.32.85", 27015), timeout=60) as server:
-            details = server.info()["server_name"]
-            large_text = server.info()["map"]
-            large_image = map_name_to_map_image(server.info()["map"])
-            if server.info()["player_count"] == 0: party_size = (server.info()["player_count"] + 1, server.info()["max_players"]) # player_count can be off slightly, which messes with everything
-            else: party_size = (server.info()["player_count"], server.info()["max_players"])
+        if server_ip != "":
+            with ServerQuerier(("167.114.32.85", 27015), timeout=60) as server:
+                details = server.info()["server_name"]
+                large_text = server.info()["map"]
+                large_image = map_name_to_map_image(server.info()["map"])
+                if server.info()["player_count"] == 0: party_size = (server.info()["player_count"] + 1, server.info()["max_players"]) # player_count can be off slightly, which messes with everything
+                else: party_size = (server.info()["player_count"], server.info()["max_players"])
+
+                RPC.update(
+                    small_image="tf2button", 
+                    small_text="TF2 Rich Presence by cyclowns#1440",
+                    large_image=large_image,
+                    large_text=large_text,
+                    details=details,
+                    state="Playing",
+                    party_size=party_size
+                )
+                server.close()
+        else:
             RPC.update(
                 small_image="tf2button", 
-                small_text="TF2 Rich Presence by cyclowns#1440",
-                large_image=large_image,
-                large_text=large_text,
-                details=details,
-                state="Playing",
-                party_size=party_size
+                small_text="TF2 Rich Presence by cyclowns#1440", 
+                large_image="mainmenu",
+                large_text="Main Menu",
+                details="Main Menu"
             )
-
-            time.sleep(15)
+        time.sleep(15)
     except NoResponseError:
-        print("Got no response ")
-
-
+        print("Got no response from server! Resetting IP...")
