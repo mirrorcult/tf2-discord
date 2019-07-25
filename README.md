4f920589425320ecf2ea81d52e01e0391c46c424
cyclowns <notzombiedude@gmail.com>
2019-07-24 20:04:17 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tf2-rich-presence
+
+python script that provides a discord rich presence description for TF2 servers
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from config import *
+from pypresence import Presence
+import valve.source
+import valve.source.a2s
+import valve.source.master_server
+import time
+
+print(client_id)
