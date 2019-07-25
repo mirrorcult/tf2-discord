4be8881149afd45fe75d29cc83371063c112ba31
cyclowns <notzombiedude@gmail.com>
2019-07-24 20:16:32 -0700

    testing RPC

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 client_id = "451950787996680192"
 
-"maps" = {
+maps = {
 		"surf_": "surf",
 		"jump_": "jump",
 		"bhop_": "bhop",
--- src/main.py
+++ src/main.py
@@ -4,5 +4,14 @@ import valve.source
 import valve.source.a2s
 import valve.source.master_server
 import time
+import random
 
-print(client_id)
+RPC = Presence(client_id)
+RPC.connect()
+
+rand = ["111", "222", "333"]
+
+# Main loop
+while True:
+    RPC.update(details="testing:", state=random.choice(rand))
+    time.sleep(15)
