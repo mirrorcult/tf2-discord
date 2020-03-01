1b37a3f195597641d3bbef5381738a049caa1226
cyclowns <notzombiedude@gmail.com>
2020-03-01 12:37:07 -0700

    Update build_windows.bat

--- dist/build_windows.bat
+++ dist/build_windows.bat
@@ -1,12 +1,14 @@
-if exist %~dp0windows del %~dp0windows
+@echo off
+
+if exist %~dp0windows rmdir %~dp0windows
 mkdir %~dp0windows
-xcopy "%~dp0..\build_files\windows\*" "windows" /i
-xcopy "%~dp0..\LICENSE" "windows" /i
-xcopy "%~dp0..\README.md" "windows" /i
+xcopy "%~dp0..\build_files\windows\*" "%~dp0windows" /i
+xcopy "%~dp0..\LICENSE" "%~dp0windows" /i
+xcopy "%~dp0..\README.md" "%~dp0windows" /i
 
 pip install -r "%~dp0..\requirements.txt"
 
-pyinstaller %~dp0..\src\main.py --clean --noconsole --distpath windows
+pyinstaller %~dp0..\src\main.py --clean --noconsole --distpath %~dp0windows
 ren "%~dp0windows\main\" "tf2-discord"
 
-del main.spec
+del %~dp0main.spec
