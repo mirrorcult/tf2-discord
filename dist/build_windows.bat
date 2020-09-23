b11511e956b8819bc4d3092bf53a4b5aced910d4
cyclowns <cyclowns@users.noreply.github.com>
2020-09-22 19:16:21 -0700

    emergency build fix

--- dist/build_windows.bat
+++ dist/build_windows.bat
@@ -4,7 +4,7 @@ if exist tf2disc-windows ( del /f /q /s tf2disc-windows\*.* > NUL && rmdir /q /s
 
 pip install -r requirements.txt
 
-xcopy "%~dp0"..\src\*.py" .
+xcopy "%~dp0..\src\*.py" .
 xcopy "%~dp0..\assets\tf2discord.ico" .
 python setup.pyw build
 del *.py
